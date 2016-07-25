abb17a46165abbe2ffccf1ceb3fecce764ee9790
jcl <jcl@MyDeby>
2016-07-25 17:28:35 -0300

    Apenas reformatação do código

--- src/main/java/jcl/kdd/zoo/TaxonomicImporter.java
+++ src/main/java/jcl/kdd/zoo/TaxonomicImporter.java
@@ -12,17 +12,19 @@ import java.sql.Connection;
  * @author Dalva
  */
 public class TaxonomicImporter {
+
     //Kingdom
     //Rank
-    public final static String taxon_unit_types_SQL="select * from taxon_unit_types";
+    public final static String taxon_unit_types_SQL = "select * from taxon_unit_types";
     //Taxonomy Author
-    public final static String _SQL="";
+    public final static String _SQL = "";
     //Taxonomy
     public final static String taxonomic_units_SQL = "select tsn,parent_tsn,taxon_autor_id,kingdom_id,rank_id,complete_name from taxonomic_units";
-    
-    public TaxonomicImporter(Connection c){
-        
+
+    public TaxonomicImporter(Connection c) {
+
     }
-    public void run(){
+
+    public void run() {
     }
 }
